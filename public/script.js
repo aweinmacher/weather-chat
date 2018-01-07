ad84a56c93a762fe086995784b66090b488099c8
Anna W <aweinmacher@gmail.com>
2018-01-07 14:18:48 +0200

    local storage

--- public/script.js
+++ public/script.js
@@ -1,21 +1,47 @@
+var STORAGE_ID = 'weather-chat';
+var results = [];
+var saveToLocalStorage = function () {
+    localStorage.setItem(STORAGE_ID, JSON.stringify(results));
+  }
+var getFromLocalStorage = function () {
+    return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+  }
+
+var renderResults = function () {
+        $(".results").empty();
+        results = getFromLocalStorage();
+        var source = $("#weather-template").html();
+        var template = Handlebars.compile(source);
+        var weatherData = {"cities": results};
+        var newHTML = template(weatherData);
+        $(".results").append(newHTML);
+}
+
+
 var fetch = function () {
-    var title = $("#city").val();
-    var buildUrl = `api.openweathermap.org/data/2.5/weather?q=${city}`
+    var city = $("#city").val();
+    var buildUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d703871f861842b79c60988ccf3b17ec`
     // AJAX
     $.ajax({
       method: "GET",
       url: buildUrl,
       success: function (data) {
         console.log(data);
-        // $(".book").empty();
-        // // HANDLEBARS
-        // var source = $("#weather-template").html();
-        // var template = Handlebars.compile(source);
-        // var newHTML = template(data);
-        // $(".results").append(newHTML); 
-        
-        
-        // END OF HANDLEBARS
+        var result = {
+            city: data.name,
+            kelvin: data.main.temp,
+            celsius: Math.round(data.main.temp - 273.15),
+            fahrenheit: Math.round(data.main.temp*9/5 - 459.67),
+            day: new Date().getDate(),
+            month: new Date().getMonth()+1,
+            year: new Date().getFullYear(),
+            hours: new Date().getHours(),
+            min: new Date().getMinutes()
+        }
+        results.push(result);
+        saveToLocalStorage();
+        console.log(results);
+        renderResults();
       },
       error: function (jqXHR, textStatus, errorThrown) {
         console.log(textStatus);
@@ -25,3 +51,4 @@ var fetch = function () {
   };
   
   $(".search-city").on("click", fetch);
+  renderResults();
